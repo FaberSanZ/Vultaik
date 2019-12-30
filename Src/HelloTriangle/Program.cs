4d988644f2772a8176cab99012efaf08797616c4
Faber Sanchez <faber.reach@gmail.com>
2019-12-30 02:05:33 -0500

    Add projects

---
+++ Src/HelloTriangle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Src/HelloWindow/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWindow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zeckoxe.Desktop.Window window = new Zeckoxe.Desktop.Window("", 800, 600);
+            {
+
+            };
+
+
+
+
+            window.RenderLoop(() => { });
+        }
+    }
+}
