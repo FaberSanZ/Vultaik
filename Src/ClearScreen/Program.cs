37e09edc02ba55afeff2c4092f1a351ca1431b65
Mr Faber SanZ <faber.software.no@gmail.com>
2024-07-09 23:32:18 -0500

    Add reusable framework for examples

--- Src/ClearScreen/Program.cs
+++ Src/ClearScreen/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Vultaik;
+
+using var App = new Sample();
+App.Run();
+
+
+public class Sample : Application, IDisposable
+{
+
+
+    public override void Initialize()
+    {
+
+
+    }
+
+
+    public override void Update(TimerTick time)
+    {
+        //Console.WriteLine(time.TotalTime);
+    }
+
+
+    public override void Draw(TimerTick time)
+    {
+
+    }
+
+    public void Dispose()
+    {
+
+    }
+}
--- Src/Vultaik/Application.cs
+++ Src/Vultaik/Application.cs
@@ -4,13 +4,76 @@
 
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Vultaik
 {
-    public  class Application
+    public class Application
     {
+
+
+        public Application()
+        {
+
+            Window = new(800, 600, "Vultaik");
+
+            Time = new();
+            Time.Reset();
+        }
+
+
+
+
+
+
+        public Window Window { get; set; }
+        public TimerTick Time { get; }
+
+
+
+        public void Run()
+        {
+
+            Initialize();
+            LoadContentAsync();
+
+            Time.Tick(); // update
+            Window?.Show();
+            //Window!.Resize += ExampleBase_Resize;
+            Window.RenderLoop(() =>
+            {
+                Update(Time);
+                Draw(Time);
+                Time.Tick();
+            });
+        }
+
+
+        public virtual void Initialize()
+        {
+        }
+
+
+        public virtual Task LoadContentAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public virtual void Draw(TimerTick time)
+        {
+        }
+
+        public virtual void Update(TimerTick time)
+        {
+        }
+
+        public virtual void Resize(int width, int height)
+        {
+
+        }
+
     }
 }
