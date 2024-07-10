3af046311757ba6ab6fc3b378486976e8fbd70fb
Mr Faber SanZ <faber.software.no@gmail.com>
2024-07-09 23:28:43 -0500

    Add a basic timer (TimerTick)

--- Src/Vultaik/Application.cs
+++ Src/Vultaik/Application.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Copyright (c) Faber Leonardo. All Rights Reserved. https://github.com/FaberSanZ
+// This code is licensed under the MIT license (MIT) (http://opensource.org/licenses/MIT)
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
